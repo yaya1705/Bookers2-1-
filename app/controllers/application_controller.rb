a45ad72254da38a6d1a2002aa03a1ff54d5ecdee
yaya1705 <yaya0517k@gmail.com>
2022-09-10 07:26:21 +0000

    [Add]header+footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  # ユーザ登録、ログイン認証などが使われる前にonfigure_permitted_parametersメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # devise_parameter_sanitizer.permitメソッドを使うことで
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+
   devise_for :users
+  root to: "homes#top"
+  get "homes/about" => "homes#about", as: "about"
+  resources :users, only:[:index,:show,:edit,:create,:update,:destroy]
+  resources :books, only:[:index,:show,:edit,:create,:update,:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
