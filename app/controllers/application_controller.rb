d9736e289f32d46e5826e9be602f00a140a91385
yaya1705 <yaya0517k@gmail.com>
2022-09-10 10:33:14 +0000

    [Add]aboutページ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
   # ユーザ登録、ログイン認証などが使われる前にonfigure_permitted_parametersメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
+
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
   protected
 
   # devise_parameter_sanitizer.permitメソッドを使うことで
