56758477e750b316202922e31a95d0663078f763
yaya1705 <yaya0517k@gmail.com>
2022-09-11 18:26:22 +0000

    [Add]user/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   before_action :ensure_correct_user, only:[:edit,:update]
-  
+
   def index
+    @users = User.all
 
   end
 
@@ -11,7 +13,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
      @user = User.find(params[:id])
      if @user.update(user_params)
@@ -26,12 +28,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name,:introduction,:profile_image)
   end
-  
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
