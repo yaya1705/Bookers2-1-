4fa04d4bd6a4e9db7f94cc38c2b86e736431f074
yaya1705 <yaya0517k@gmail.com>
2022-09-11 18:45:35 +0000

    [Add]user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
