06e4ded4f9fc0ce8ac858a7af61d20bf51a39e62
yaya1705 <yaya0517k@gmail.com>
2022-09-19 00:24:36 +0000

    [Update]controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def index
     @user = current_user
     @book = Book.new
@@ -45,5 +46,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title,:body,:profile_image)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to books_path
+  unless
+     @user == current_user
+  end
   end
 end
